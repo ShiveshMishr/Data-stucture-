59e2fdc00ba984b7d969f032c061773fa22a7b0a
shivesh misrha <mishrashivesh396@gmail.com>
2023-02-23 00:41:42 +0530

    init

---
+++ Data-stucture-/lovebabar/practice31.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+using namespace std;
+
+int max(int arr[], int size){
+    int a = INT_MIN;
+    for(int i =0 ;i<size ; i++){
+        if(arr[i]>a){
+            a= arr[i];
+        }
+    }
+    return a;
+}
+int min(int arr[], int size)
+{
+    int a = INT_MAX;
+    for (int i = 0; i < size; i++)
+    {
+        if (arr[i] < a)
+        {
+            a = arr[i];
+        }
+    }
+    return a;
+}
+
+int main(){
+int size;
+cin>> size;
+
+int arr[100];
+for(int i=0 ; i < size  ; i++){
+    cin>> arr[i];
+}
+
+cout<< "max value in array"<< max(arr , size)<< endl;
+cout << "min value in array" << min(arr, size);
+return 0;
+}
